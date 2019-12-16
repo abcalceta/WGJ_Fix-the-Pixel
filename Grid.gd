8dbc63b6ec4d295b39517995291ab05f6ca66356
abcalceta <frances.calceta@gmail.com>
2019-12-16 15:34:37 +0800

    changed to not recursive flood fill

--- Grid.gd
+++ Grid.gd
@@ -4,6 +4,7 @@ extends Node2D
 var pixel = preload("res://Pixel.tscn")
 
 var grid = []
+var gridTemp = []
 var gridHeight = 12
 var gridWidth = 20
 var pixelGrid = []
@@ -12,13 +13,16 @@ var xOffset = 10
 var yOffset = 0
 
 var speed = 0.1
+var speedFast = 0.01
 
 func _ready():
 	for x in range(gridHeight):
 		grid.append([])
+		gridTemp.append([])
 		pixelGrid.append([])
 		for y in range(gridWidth):
 			grid[x].append(0)
+			gridTemp[x].append(0)
 			var newPixel = pixel.instance()
 			add_child(newPixel)
 			pixelGrid[x].append(newPixel)
@@ -28,31 +32,59 @@ func _ready():
 
 	#floodFill(1,1,0,1)
 	for x in range(gridHeight):
-		grid[x][4] = 1
+		#grid[x][4] = 1
+		#gridTemp[x][4] = 1
+		pass
 	
 
 
 func _process(delta):
+	#print($FillTimer.time_left)
 	pass
+	
+	
+func fillBoardFast(x, y, targetColor, replacementColor, grid):
+	floodFillFast(x, y, targetColor, replacementColor, grid)
+	
+func floodFillFast(x, y, targetColor, replacementColor, grid):
+	if x>=gridHeight or x<0 or y>=gridWidth or y<0:
+		return
+	elif targetColor == replacementColor:
+		return
+	elif grid[x][y] != targetColor:
+		return
+	else:
+		grid[x][y] = replacementColor
+		
+	var Q = []
+	Q.append([x,y])
+	while Q.size()>0:
+		var n = Q[0]
+		print(Q[0])
+		Q.erase(n)
+		if not n[0]+1>=gridHeight:
+			if grid[n[0]+1] [n[1]] == targetColor:
+				grid[n[0]+1] [n[1]] = replacementColor
+				Q.append([n[0]+1,n[1]])
+		if not n[0]-1<0 :
+			if grid[n[0]-1] [n[1]] == targetColor:
+				grid[n[0]-1][n[1]] = replacementColor
+				Q.append([n[0]-1,n[1]])
+		if not  n[1]+1>=gridWidth :
+			if grid[n[0]] [n[1]+1] == targetColor:
+				grid[n[0]][n[1]+1] = replacementColor
+				Q.append([n[0],n[1]+1])
+		if not n[1]-1<0:
+			if grid[n[0]] [n[1]-1] == targetColor:
+				grid[n[0]] [n[1]-1] = replacementColor
+				Q.append([n[0],n[1]-1])
+	return
 
-func fillBoard(x, y, targetColor, replacementColor):
-	floodFill(x, y, targetColor, replacementColor)
 
+func fillBoardSlow(x, y, targetColor, replacementColor, grid):
+	floodFillSlow(x, y, targetColor, replacementColor, grid)
 
-
-
-func floodFill(x, y, targetColor, replacementColor):
-	globals.fillSize += 1
-	print(globals.fillSize)
-	#Flood-fill (node, target-color, replacement-color):
-	 #1. If target-color is equal to replacement-color, return.
-	 #2. ElseIf the color of node is not equal to target-color, return.
-	 #3. Else Set the color of node to replacement-color.
-	 #4. Perform Flood-fill (one step to the south of node, target-color, replacement-color).
-	    #Perform Flood-fill (one step to the north of node, target-color, replacement-color).
-	    #Perform Flood-fill (one step to the west of node, target-color, replacement-color).
-	    #Perform Flood-fill (one step to the east of node, target-color, replacement-color).
-	 #5. Return.
+func floodFillSlow(x, y, targetColor, replacementColor, grid):
 	if x>=gridHeight or x<0 or y>=gridWidth or y<0:
 		return
 	elif targetColor == replacementColor:
@@ -71,7 +103,7 @@ func floodFill(x, y, targetColor, replacementColor):
 	yield(t, "timeout")
 	
 	
-	floodFill(x+1, y, targetColor, replacementColor)
+	floodFillSlow(x+1, y, targetColor, replacementColor, grid)
 	var t1 = Timer.new()
 	t1.set_wait_time(speed)
 	t1.set_one_shot(true)
@@ -80,7 +112,7 @@ func floodFill(x, y, targetColor, replacementColor):
 	yield(t1, "timeout")
 	
 	
-	floodFill(x-1, y, targetColor, replacementColor)
+	floodFillSlow(x-1, y, targetColor, replacementColor, grid)
 	var t2 = Timer.new()
 	t2.set_wait_time(speed)
 	t2.set_one_shot(true)
@@ -89,7 +121,7 @@ func floodFill(x, y, targetColor, replacementColor):
 	yield(t2, "timeout")
 	
 	
-	floodFill(x, y+1, targetColor, replacementColor)
+	floodFillSlow(x, y+1, targetColor, replacementColor, grid)
 	var t3 = Timer.new()
 	t3.set_wait_time(speed)
 	t3.set_one_shot(true)
@@ -98,7 +130,7 @@ func floodFill(x, y, targetColor, replacementColor):
 	yield(t3, "timeout")
 	
 	
-	floodFill(x, y-1, targetColor, replacementColor)
+	floodFillSlow(x, y-1, targetColor, replacementColor, grid)
 	var t4 = Timer.new()
 	t4.set_wait_time(speed)
 	t4.set_one_shot(true)
@@ -118,14 +150,16 @@ func _on_Timer_timeout():
 		for y in range(gridWidth):
 			if pixelGrid[x][y].isToggled == true:
 				globals.noClicks = true
-				globals.fillSize = 0
-				fillBoard(x,y, grid[x][y], pixelGrid[x][y].color)
-				$Timer.stop()
-				if $ClickTimer.time_left==0:
-					$ClickTimer.wait_time = globals.fillSize*0.1
-					$ClickTimer.start()
+				$FillTimer.start()
+				fillBoardFast(x,y, grid[x][y], pixelGrid[x][y].color, grid)
+				$FillTimer.stop()
+				#if $ClickTimer.time_left==0:
+				#	$ClickTimer.wait_time = (1-$FillTimer.time_left)*10
+				#	$ClickTimer.start()
+				#fillBoardSlow(x,y, grid[x][y], pixelGrid[x][y].color, grid)
 				pixelGrid[x][y].isToggled = false
 			pixelGrid[x][y].color = grid[x][y]
+			
 	$Timer.start()
 
 func _on_ClickTimer_timeout():
